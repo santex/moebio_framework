cc62860538ddd364b76b13e294ea62daff5492fc
Jeff Clark <Jeff@neoformix.com>
2015-11-28 14:13:22 -0500

    bug fix in createRandomBimodalDistribution when randomSeed==0

--- src/operators/numeric/numberList/NumberListGenerators.js
+++ src/operators/numeric/numberList/NumberListGenerators.js
@@ -129,7 +129,7 @@ NumberListGenerators.createRandomBimodalDistribution = function(nValues,min,max,
   mode2 = (mode2 == null) ? .75 : mode2;
   lambda = (lambda == null) ? 1 : lambda;
   balance = (balance == null) ? .5 : balance;
-  if(randomSeed)
+  if(randomSeed!=null)
     NumberOperators.randomSeed(randomSeed);
   var nL = new mo.NumberList();
   var i;
