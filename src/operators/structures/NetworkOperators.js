7983b4ce8a4a32176b99768e340eb31e7dfaec04
Jeff Clark <Jeff@neoformix.com>
2015-12-29 20:48:32 -0500

    Simplify interface to addEigenvectorCentralityToNodes and add comment block to both new centrality measures

--- src/operators/structures/NetworkOperators.js
+++ src/operators/structures/NetworkOperators.js
@@ -1522,7 +1522,16 @@ NetworkOperators.getAdjacencyMap = function(network,bDirected,bReversed,bStochas
   return map;
 }
 
-NetworkOperators.addEigenvectorCentralityToNodes = function(network, bNormalized, bReversed, nIterations, fTolerance) {
+/**
+ * Adds eigenvectorCentrality as a property to the network nodes.
+ * Nodes with no incoming connections have a score of zero.
+ *
+ * @param {Network} network
+ * @param {Boolean} bNormalized=true (default), scale so maximum eigenvectorCentrality is 1
+ * @param {Boolean} bReversed=true (default), use outgoing relations
+ * tags:analytics,transformative
+ */
+NetworkOperators.addEigenvectorCentralityToNodes = function(network, bNormalized, bReversed) {
   // derived from Graph.js inside http://www.clips.ua.ac.be/pages/pattern
   /* Eigenvector centrality for nodes in the graph (cfr. Google's PageRank).
    * Eigenvector centrality is a measure of the importance of a node in a directed network. 
@@ -1536,8 +1545,8 @@ NetworkOperators.addEigenvectorCentralityToNodes = function(network, bNormalized
   if(network == null) return;
   if(bNormalized === undefined) bNormalized = true;
   if(bReversed   === undefined) bReversed   = true;
-  if(nIterations === undefined) nIterations = 100;
-  if(fTolerance  === undefined) fTolerance  = 0.0001;
+  var nIterations = 100;
+  var fTolerance  = 0.0001;
   var rating = {};
   function normalize(vector) {
     var v = []; for(var k in vector) v.push(vector[k]);
@@ -1580,6 +1589,112 @@ NetworkOperators.addEigenvectorCentralityToNodes = function(network, bNormalized
   // node weight is 0 because eigenvectorCentrality() did not converge
   for(var i=0;i<network.nodeList.length;i++){
     network.nodeList[i].eigenvectorCentrality=0;
+  }
+};
+
+/**
+ * Adds betweennessCentrality as a property to the network nodes.
+ * Nodes in high-density areas will get a good score.
+ *
+ * @param {Network} network
+ * @param {Boolean} bNormalized=true (default), scale so maximum betweennessCentrality is 1
+ * @param {Boolean} bDirected=false (default), count relations in both directions
+ * tags:analytics,transformative
+ */
+NetworkOperators.addBetweennessCentralityToNodes = function(network, bNormalized, bDirected) {
+  // derived from Graph.js inside http://www.clips.ua.ac.be/pages/pattern
+  /* Betweenness centrality for nodes in the graph.
+   * Betweenness centrality is a measure of the number of shortests paths that pass through a node.
+   * Nodes in high-density areas will get a good score.
+   */
+  // Ulrik Brandes, A Faster Algorithm for Betweenness Centrality,
+  // Journal of Mathematical Sociology 25(2):163-177, 2001,
+  // http://www.inf.uni-konstanz.de/algo/publications/b-fabc-01.pdf
+  // Based on: Dijkstra's algorithm for shortest paths modified from Eppstein.
+  // Based on: NetworkX 1.0.1: Aric Hagberg, Dan Schult and Pieter Swart.
+  // http://python-networkx.sourcearchive.com/documentation/1.0.1/centrality_8py-source.html
+  if(network == null) return;
+  if(bNormalized === undefined) bNormalized = true;
+  if(bDirected === undefined) bDirected = false;
+
+  var Heap = Class.extend({
+      init: function() {
+          /* Items in the heap are ordered by weight (i.e. priority).
+           * Heap.pop() returns the item with the lowest weight.
+           */
+          this.k = [];
+          this.w = [];
+          this.length = 0;
+      },
+      push: function (key, weight) {
+          var i = 0; while (i <= this.w.length && weight < (this.w[i]||Infinity)) i++;
+          this.k.splice(i, 0, key);
+          this.w.splice(i, 0, weight);
+          this.length += 1;
+          return true;            
+      },
+      pop: function () {
+          this.length -= 1;
+          this.w.pop(); return this.k.pop();
+      }
+  });
+
+  var W = NetworkOperators.getAdjacencyMap(network,null,true);
+  var b = {}; for(var i=0;i<network.nodeList.length;i++) b[network.nodeList[i].id] = 0.0;
+  var dist,pred,v;
+  for(i=0;i<network.nodeList.length;i++) {
+    var id = network.nodeList[i].id;
+    var Q = new Heap(); // Use Q as a heap with [distance, node id] lists.
+    var D = {}; // Dictionary of final distances.
+    var P = {}; // # Dictionary of paths.
+    for(var j=0;j<network.nodeList.length;j++) P[network.nodeList[j].id]=[];
+    var seen = {id: 0};
+    Q.push([0, id, id], 0);
+    var S = [];
+    var E = {};
+    for(var j=0;j<network.nodeList.length;j++) E[network.nodeList[j].id]=0; // sigma
+    E[id] = 1.0;
+    while(Q.length) {
+      var q = Q.pop(); dist=q[0]; pred=q[1]; v=q[2];
+      if(v in D) continue;
+      D[v] = dist;
+      S.push(v);
+      E[v] += E[pred];
+      for (var w in W[v]) {
+        var vw_dist = D[v] + W[v][w];
+        if(!(w in D) && (!(w in seen) || vw_dist < seen[w])) {
+          seen[w] = vw_dist;
+          Q.push([vw_dist, v, w], vw_dist);
+          P[w] = [v];
+          E[w] = 0.0;
+        } else if (vw_dist == seen[w]) { // Handle equal paths.
+          P[w].push(v);
+          E[w] = E[w] + E[v];
+        }
+      }
+    }
+    var d = {}; for(var j=0;j<network.nodeList.length;j++) d[network.nodeList[j].id]=0;
+    while (S.length) {
+      var w = S.pop();
+      for (var j=0; j < P[w].length; j++) {
+        v = P[w][j];
+        d[v] += (1 + d[w]) * E[v] / E[w];
+      }
+      if (w != id) {
+        b[w] += d[w];
+      }
+    }
+  }
+  // Normalize between 0 and 1.
+  if(bNormalized){
+    var vals = []; for(var k in b) vals.push(b[k]);
+    var m=Math.max.apply(Math, vals) || 1;
+    for(var id in b) b[id] /= m;
+  }
+  // set property on nodes
+  for(var id in b){
+    var node = network.nodeList.getNodeById(id);
+    node.betweennessCentrality = b[id];
   }
 };
 
