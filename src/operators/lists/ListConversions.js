39c646e4922fc9901d24677c9e29480d6b1f201d
Santiago Ortiz <s@moebio.com>
2016-04-03 17:16:16 +0200

    toNumberList and toStringList moved to List

--- src/operators/lists/ListConversions.js
+++ src/operators/lists/ListConversions.js
@@ -10,49 +10,52 @@ import StringList from "src/dataTypes/strings/StringList";
 function ListConversions() {}
 export default ListConversions;
 
+
+
 /**
  * Converts the List into a NumberList.
- *
  * @param  {List} list
  * @return {NumberList}
- * tags:conversion
+ * tags:deprecated
  */
 ListConversions.toNumberList = function(list) {
   if(list==null) return;
+  return list.toNumberList();
 
-  var numberList = new NumberList();
-  var l = list.length;
-  var i;
+  // var numberList = new NumberList();
+  // var l = list.length;
+  // var i;
 
-  numberList.name = list.name;
-  for(i = 0; i<l; i++) {
-    numberList[i] = Number(list[i]);
-  }
-  return numberList;
+  // numberList.name = list.name;
+  // for(i = 0; i<l; i++) {
+  //   numberList[i] = Number(list[i]);
+  // }
+  // return numberList;
 };
 
 /**
  * Converts the List into a StringList.
- *
  * @param  {List} list
  * @return {StringList}
- * tags:conversion
+ * tags:deprecated
  */
 ListConversions.toStringList = function(list) {
   if(list==null) return;
+  return list.toStringList();
 
-  var l = list.length;
-  var i;
-  var stringList = new StringList();
+  // var l = list.length;
+  // var i;
+  // var stringList = new StringList();
 
-  stringList.name = list.name;
-  for(i = 0; i<l; i++) {
-    stringList[i] = String(list[i]);
-    // if(typeof list[i] == 'number') {
-    //   stringList[i] = String(list[i]);
-    // } else {
-    //   stringList[i] = list[i].toString();
-    // }
-  }
-  return stringList;
+  // stringList.name = list.name;
+  // for(i = 0; i<l; i++) {
+  //   stringList[i] = String(list[i]);
+  //   // if(typeof list[i] == 'number') {
+  //   //   stringList[i] = String(list[i]);
+  //   // } else {
+  //   //   stringList[i] = list[i].toString();
+  //   // }
+  // }
+  // return stringList;
 };
+
