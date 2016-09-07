c7532f40a6cceba9920c871dc951150044681485
Jeff Clark <Jeff@neoformix.com>
2016-09-07 16:57:43 -0400

    StringConversions.stringToObject - improvements to handle common quasi-json formats like a list of objects without enclosing brackets or commas in between objects

--- src/operators/strings/StringConversions.js
+++ src/operators/strings/StringConversions.js
@@ -21,6 +21,34 @@ StringConversions.stringToObject = function(string) {
   try {
     return JSON.parse(string);
   } catch(err) {
+    // fall through and try some common quasi-json formats
+  }
+  if(string == null) return null;
+  // often we have a list of objects on separate lines without enclosing [] or commas between
+  var aObjs = string.split(/\r?\n/);
+  var s='[';
+  var sAdded, bAddedItem = false;
+  for(var i = 0; i<aObjs.length; i++){
+    if(aObjs[i].trim().length == 0) continue;
+    if(aObjs[i] == '{'){
+      // assume each object definition spread across multiple lines
+      sAdded = '';
+      while(aObjs[i] != '}' && i < aObjs.length){
+        sAdded += ' ' + aObjs[i];
+        i++;
+      }
+      sAdded += '}';
+    }
+    else
+      sAdded = aObjs[i];
+    sAdded = (bAddedItem ? ',\r\n' : '\r\n') + sAdded;
+    bAddedItem = true;
+    s += sAdded;
+  }
+  s += '\r\n]';
+  try {
+    return JSON.parse(s);
+  } catch(err) {
     return null;
   }
 };
